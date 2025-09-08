3f1b5b2beb3ac4c27468394cfd42eebc19c57e78
willyan santos tomaz e silva <willyantomaz2015@gmail.com>
2025-09-07 21:40:04 -0300

    initial-api

---
+++ gift_list_api/src/main/java/br/com/project/gift_list_api/GiftListApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.gift_list_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftListApiApplication.class, args);
+	}
+
+}
---
+++ gift_list_api/src/test/java/br/com/project/gift_list_api/GiftListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.gift_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
